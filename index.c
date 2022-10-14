09510c8595b3118cd28d020a9701265112fbf64f
Samet Çiftçi <temasictfic@gmail.com>
2022-10-14 11:58:55 +0300

    gnl added

--- index.c
+++ index.c
@@ -6,7 +6,7 @@
 /*   By: sciftci <sciftci@student.42kocaeli.com.tr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 02:44:50 by sciftci           #+#    #+#             */
-/*   Updated: 2022/10/14 11:38:56 by sciftci          ###   ########.fr       */
+/*   Updated: 2022/10/14 11:58:08 by sciftci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,33 +58,33 @@ int	get_lowest_order_index(t_stack **stack)
 }
 
 /*
-*	Picks the best target position in stack A for the given index of
-*	an element in stack B. First checks if the index of the B element can
-*	be placed somewhere in between elements in stack A, by checking whether
-*	there is an element in stack A with a bigger index. If not, it finds the
-*	element with the smallest index in A and assigns that as the target position.
-*	--- Example:
-*		target_pos starts at INT_MAX
-*		B index: 3
-*		A contains indexes: 9 4 2 1 0
-*		9 > 3 && 9 < INT_MAX 	: target_pos updated to 9
-*		4 > 3 && 4 < 9 			: target pos updated to 4
-*		2 < 3 && 2 < 4			: no update!
-*	So target_pos needs to be the position of index 4, since it is
-*	the closest index.
-*	--- Example:
-*		target_pos starts at INT_MAX
-*		B index: 20
-*		A contains indexes: 16 4 3
-*		16 < 20					: no update! target_pos = INT_MAX
-*		4  < 20					: no update! target_pos = INT_MAX
-*		3  < 20					: no update! target_pos = INT_MAX
-*	... target_pos stays at INT MAX, need to switch strategies.
-*		16 < 20					: target_pos updated to 20
-*		4  < 20					: target_pos updated to 4
-*		3  < 20					: target_pos updated to 3
-*	So target_pos needs to be the position of index 3, since that is
-*	the "end" of the stack.
+**	Picks the best target_index in stack_a for the given order of
+**	an element in stack_b. First checks if the order of stack_b element can
+**	be placed somewhere in between elements in stack_a, by checking whether
+**	there is an element in stack_a with a bigger order. If not, it finds the
+**	element with the smallest order in stack_a and assigns that as the target_index.
+**	--- Example:
+**		target_index starts at INT_MAX
+**		B order: 3
+**		A contains orders: 9 4 2 1 0
+**		9 > 3 && 9 < INT_MAX 	: target_index updated to 9
+**		4 > 3 && 4 < 9 			: target_index updated to 4
+**		2 < 3 && 2 < 4			: no update!
+**	So target_index needs to be the index of order 4, since it is
+**	the closest order.
+**	--- Example:
+**		target_index starts at INT_MAX
+**		B order: 20
+**		A contains orders: 16 4 3
+**		16 < 20					: no update! target_index = INT_MAX
+**		4  < 20					: no update! target_index = INT_MAX
+**		3  < 20					: no update! target_index = INT_MAX
+**	... target_index stays at INT MAX, need to switch strategies.
+**		16 < 20					: target_index updated to 20
+**		4  < 20					: target_index updated to 4
+**		3  < 20					: target_index updated to 3
+**	So target_index needs to be the index of order 3, since that is
+**	the "end" of the stack.
 */
 static int	get_target_index(t_stack **stack_a, int b_order, int target_order,
 		int target_index)
